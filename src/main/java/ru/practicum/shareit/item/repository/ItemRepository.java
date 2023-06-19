8ffb89b69faf6699a59cb1f95d20832e541af6a9
AraShock <107464978+AraShock@users.noreply.github.com>
2023-06-19 20:35:32 +0300

    add-item-requests rew.1

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,18 +1,23 @@
 package ru.practicum.shareit.item.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
+    List<Item> findAllByOwnerId(Pageable pageable, Long ownerId);
 
-    List<Item> findAllByUserIdOrderById(long userId);
+    List<Item> findAllByNameOrDescriptionContainingIgnoreCaseAndAvailableTrue(Pageable pageable,
+                                                                              String name,
+                                                                              String description);
 
-    @Query(value = "SELECT i FROM Item i WHERE lower(i.name) like %?1% OR lower(i.description) like %?1% " +
-            "AND i.available=TRUE")
-    List<Item> findByNameOrDescriptionLike(String text);
+    Boolean existsItemByOwnerId(Long ownerId);
 
-    void deleteById(long itemId);
+    @Query("SELECT i.id FROM Item AS i " +
+            "JOIN User As u ON i.owner.id=u.id " +
+            "WHERE i.owner.id = ?1")
+    List<Long> findAllItemIdByOwnerId(Long ownerId);
 }
