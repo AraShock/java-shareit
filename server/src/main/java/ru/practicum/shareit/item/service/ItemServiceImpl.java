a2ac50ea25133c664bd3dc54177093ad6356df2e
AraShock <107464978+AraShock@users.noreply.github.com>
2023-06-28 22:30:28 +0300

    add-docker v2.1

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -72,10 +72,10 @@ public class ItemServiceImpl implements ItemService {
         if (item.getOwner().getId().equals(userId)) {
             itemDtoResponse.setLastBooking(mapper
                     .mapToBookingShortDto(bookings
-                            .findFirstByItemIdAndStartAfterAndStatusOrderByStartAsc(item, Status.APPROVED).orElse(null)
+                            .findFirstByItemAndStatusIsOrderByStartAsc(item, Status.APPROVED).orElse(null)
                     ));
             itemDtoResponse.setNextBooking(mapper.mapToBookingShortDto(bookings
-                    .findFirstByItemIdAndStartBeforeAndStatusOrderByStartDesc(item, Status.APPROVED).orElse(null)));
+                    .findFirstByItemAndStatusIsOrderByEndDesc(item, Status.APPROVED).orElse(null)));
             return itemDtoResponse;
         }
         return itemDtoResponse;
@@ -92,9 +92,9 @@ public class ItemServiceImpl implements ItemService {
         for (Item item : findItems) {
             ItemDtoResponse itemDtoResponse = mapper.mapToItemDtoResponse(item);
             itemDtoResponse.setLastBooking(mapper.mapToBookingShortDto(
-                    bookings.findFirstByItemIdAndStartAfterAndStatusOrderByStartAsc(item, Status.APPROVED).orElse(null)));
+                    bookings.findFirstByItemAndStatusIsOrderByStartAsc(item, Status.APPROVED).orElse(null)));
             itemDtoResponse.setNextBooking(mapper.mapToBookingShortDto(
-                    bookings.findFirstByItemIdAndStartBeforeAndStatusOrderByStartDesc(item, Status.APPROVED).orElse(null)));
+                    bookings.findFirstByItemAndStatusIsOrderByEndDesc(item, Status.APPROVED).orElse(null)));
             personalItems.add(itemDtoResponse);
 
         }
